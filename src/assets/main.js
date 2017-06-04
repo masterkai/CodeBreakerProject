c4cba0064f1b725545575eb68d8f4ae4f522de09
masterkai <kai0919@hotmail.com>
2017-06-04 17:17:01 +0800

    step 12

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,40 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if(answer.value==''||attempt.value=''){
+        setHiddenFields();
+    }
+
+    // Call the validateInput function from the guess function
+    if(!validateInput(input.value)){
+        return;
+    }
+    attempt.value++;
+
 }
-console.log('hello World!');
-//implement new functions here
+//function takes user input, check against the answer, and displays results
+function getResults(input) {
+    let correct = 0;
+    let html = <div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">;
+}
+
+// implement new functions here
+function setHiddenFields() {
+    answer.value = Math.floor(Math.random()*10000).toString();
+    while (answer.length<4){
+        answer.value = "0" + answer.value;
+    }
+    attempt.value =0;
+}
+
+function setMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input) {
+    if(input.length != 4){
+        setMessage("Guesses must be exactly 4 characters long.");
+        return false;
+    }
+    return true;
+}
