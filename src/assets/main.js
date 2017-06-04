0573fc55d691be46004df64479580c917d7b2711
masterkai <kai0919@hotmail.com>
2017-06-04 17:54:23 +0800

    getResults function complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,9 +18,29 @@ function guess() {
 function getResults(input) {
     let correct = 0;
     let html = <div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">;
+    for(i=0;i<input.length;i++){
+        if(input.charAt(i)==answer.value.charAt(i)){
+            html += '<span class="glyphicon' +
+                ' glyphicon-ok"></span>';
+            correct++;
+        }else if(answer.value.indexOf(input.charAt(i)) > -1){
+            html += '<span class="glyphicon glyphicon-transfer"></span>';
+        }else {
+            html += '<span class="glyphicon glyphicon-remove"></span>';
+        }
+    }
+    html += '</div></div>';
+
+    document.getElementById('results').innerHTML = html;
+
+    if(correct=input.length){
+        return true;
+    }else {
+        return false;
+    }
 }
 
-// implement new functions here
+//function sets hidden fields
 function setHiddenFields() {
     answer.value = Math.floor(Math.random()*10000).toString();
     while (answer.length<4){
